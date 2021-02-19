c46cf1baad709519aea1879b049a7a35a71fd68c
fdznoriega <andresfdz7@gmail.com>
2021-02-18 20:59:20 -0500

    successfully moved about page to JS

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ fetch('assets/data.json')
         console.log(data);
 
         // render html in functions!
+        renderMainPage(data);
 
 
     });
@@ -15,7 +16,7 @@ function renderMainPage(data)
     // select containers and populate them
     document.querySelector('.container').innerHTML = `
         
-        ${renderAbout(data.about)}
+        ${renderAbout(data)}
         
     `;
 }
@@ -25,34 +26,43 @@ function renderNavbar(page, data)
 {
     console.log(page, data);
 
-    return <div>This is the nav bar</div>
 }
 
 // renders about page using data from json
 function renderAbout(data)
 {
     return (
-        `<span>
-            <b>${data.about.name}</b> <br/>
-            ${data.about.email}
-            ${data.about.collegeInfo}
-            ${data.about.major}
-            ${data.about.minor}
-            <!-- icons for resume, github, and linkedin -->
-            <a href=${data.about.photo} target="_blank">
-                <i class="fas fa-user-tie"></i>
-                Resume | 
-            </a>
-            <!-- github -->
-            <a href=${data.about.github} target="_blank">
-                <i class="fab fa-github"></i> |
-            </a>
-            <!-- linked in -->
-            <a href=${data.about.linkedin} target="_blank">
-                <i class="fab fa-linkedin"></i>
-            </a>
+        `
+        <div class="row">
+            
+            <div class="col-6">
+                <img id="mySnapshot" src=${data.about.photo}>
+            </div>
+            
+            <div class="col-6">
+                <span>
+                    <b>${data.about.name}</b> <br/>
+                    ${data.about.email} <br/>
+                    ${data.about.collegeInfo} <br/>
+                    ${data.about.major} <br/>
+                    ${data.about.minor} <br/>
+                    <!-- icons for resume, github, and linkedin -->
+                    <a href=${data.about.resume} target="_blank">
+                        <i class="fas fa-user-tie"></i>
+                        Resume | 
+                    </a>
+                    <!-- github -->
+                    <a href=${data.about.github} target="_blank">
+                        <i class="fab fa-github"></i> |
+                    </a>
+                    <!-- linked in -->
+                    <a href=${data.about.linkedin} target="_blank">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </span>
+            </div>
+        
+        </div>
         `
     ); 
 }
-
-// about html
